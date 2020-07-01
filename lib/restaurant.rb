b59c997646143ac526455b2f2081df804893fb9a
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-01 13:37:23 -0600

    Rewrote restaurant.rb

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant
-  attr_reader :name, :opening_time, :dishes
+  attr_reader :opening_time, :name, :dishes
 
   def initialize(opening_time, name)
     @opening_time = opening_time
@@ -7,31 +7,14 @@ class Restaurant
     @dishes = []
   end
 
-  def add_dish(dish)
-    @dishes << dish
+  def closing_time(hours_open)
+    (@opening_time.to_i + hours_open).to_s + ":00"
   end
 
-  def closing_time(hours)
-    (@opening_time.to_i + hours).to_s + ':00'
+  def add_dish(new_dish)
+    @dishes.push(new_dish)
   end
 
-  def open_for_lunch?
-    @opening_time.to_i <= 11
-  end
 
-  def menu_dish_names
-    names_upcased = []
-    @dishes.each do |dish|
-      names_upcased << dish.upcase
-    end
-    names_upcased
-  end
 
-  def announce_closing_time(hours)
-    time = closing_time(hours) + "AM"
-    if closing_time(hours).to_i >= 12
-      time = (closing_time(hours).to_i - 12).to_s + ':00PM'
-    end
-    "#{@name} will be closing at #{time}"
-  end
 end
